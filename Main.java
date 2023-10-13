47f927f8dc1c77ad4e8611950d94f504069ee26d
lonelypenguin245 <144401532+lonelypenguin245@users.noreply.github.com>
2023-10-13 07:53:15 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SalaryWorker.Manager;
+
+import SalaryWorker.Enity.History;
+import SalaryWorker.Enity.Worker;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HI
+ */
+public class Main {
+     public static void main(String[] args) {
+        ArrayList<Worker> lw = new ArrayList<>();
+        ArrayList<History> lh = new ArrayList<>();
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.addWorker(lw);
+                    break;
+                case 2:
+                    Manager.changeSalary(lw, lh, 1);
+                    break;
+                case 3:
+                    Manager.changeSalary(lw, lh, 2);
+                    break;
+                case 4:
+                    Manager.printListHistory(lh);
+                    break;
+                case 5:
+                    return;
+            }
+        }
+    }
+}
